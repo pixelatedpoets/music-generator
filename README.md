29aef053d13d265f7e3aa2f6169a265a24311f91
Dennis Vink <dennisvink@binx.io>
2023-10-17 15:23:34 +0200

    Music generator code

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# music-generator
+# Pixelated Poets Music Generator
+
 Frontend and Backend to generate music with Facebook's MusicGen model
+Check us out on https://www.youtube.com/@PixelatedPoets/
+
+## Installation
+
+AudioCraft requires Python 3.9, PyTorch 2.0.0. To install AudioCraft, you can run the following:
+
+```bash
+git clone https://github.com/pixelatedpoets/music-generator.git
+cd music-generator
+python3.9 -m venv .venv
+source .venv/bin/activate
+pip install 'torch>=2.0'
+pip install -U audiocraft
+pip install flask
+pip install torchaudio
+```
+
+## Starting the front-end
+
+```bash
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, jsonify
+from audiocraft.models import MusicGen
+from audiocraft.data.audio import audio_write
+from io import BytesIO
+
+import base64
+import torchaudio
+
+app = Flask(__name__)
+
+model = MusicGen.get_pretrained('facebook/musicgen-melody')
+model.set_generation_params(duration=8)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.get_json()
+    promptText = data['prompt']
+    durationValue = int(data['duration'])  # Extract the duration from the request
+
+    model.set_generation_params(duration=durationValue)  # Set the duration before generating
+
+    # Generate the audio
+    wav = model.generate_unconditional(1)
+
+    # Convert the tensor to bytes
+    buffer = BytesIO()
+    torchaudio.save(buffer, wav[0].cpu(), model.sample_rate, format="wav")
+    audio_bytes = buffer.getvalue()
+
+    # Encode the audio bytes to base64
+    base64_encoded_audio = base64.b64encode(audio_bytes).decode('utf-8')
+
+    return jsonify({"audio": base64_encoded_audio})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
